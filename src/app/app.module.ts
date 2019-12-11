b2ddecef23020f9c18ac50c1f9fbae11c66662c9
Lenny Kraaijenhof <hi@lenny.kr>
2019-12-11 16:10:25 +0100

    Add interceptors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { OnboardingModule } from './modules/onboarding/onboarding.module'
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AngularSvgIconModule } from 'angular-svg-icon';
+
+import { JwtInterceptor } from '@app/helpers/jwt.interceptor';
+import { ErrorInterceptor } from '@app/helpers/error.interceptor';
 
 import { AppComponent } from './app.component';
 import { MenuMobileComponent } from './components/menu/menu-mobile/menu-mobile.component';
@@ -27,7 +30,10 @@ import { HeaderComponent } from './components/header/header.component';
     AuthModule,
     OnboardingModule,
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
