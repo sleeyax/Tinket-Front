279168458fd56e4009b6ee691fa5ef3b7dd88c08
Lenny Kraaijenhof <hi@lenny.kr>
2019-12-12 22:01:18 +0100

    Add the header component

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  @Input() title: String;
+  @Input() parentTitle: String;
+  @Input() parentRouterLink: String;
+  @Input() fixed: boolean;
 
   constructor() { }
 
