1bd322ae8382af04a6b467c866327784df3a96b5
Lenny Kraaijenhof <hi@lenny.kr>
2019-12-13 19:55:34 +0100

    Refactor my reviews

--- src/app/modules/my-reviews/my-reviews/my-reviews.component.ts
+++ src/app/modules/my-reviews/my-reviews/my-reviews.component.ts
@@ -12,26 +12,29 @@ import { filter } from 'rxjs/operators';
 })
 export class MyReviewsComponent implements OnInit {
   currentUser: User;
-  avarageScore: number;
-  isMaker = true;
-  reviews: Review[] = []
-  scores = []
+  avarageScore = 0;
+  reviews: Review[] = [];
 
-  constructor(private reviewService: ReviewService, private authenticationService: AuthenticationService) {
-    var avarage;
-    this.authenticationService.currentUser.subscribe(user => this.currentUser = user)
-    if (this.currentUser.companyProfile != null) {
-      this.isMaker = false;
-    }
-    this.reviewService.getSkills().subscribe((res) => {
-      this.reviews = res;
-      this.reviews.forEach(review => {
-        this.scores.push(review.score);
-      });
-      const average = arr => arr.reduce((p, c) => p + c, 0) / arr.length;
-      avarage = Number(average(this.scores));
-    })
-    this.avarageScore = avarage//Werkt wel
+  constructor(
+    private reviewService: ReviewService,
+    private authenticationService: AuthenticationService
+  ) {
+    this.authenticationService.currentUser
+      .subscribe(user => this.currentUser = user);
+
+    this.reviewService.getSkills().subscribe((reviews : Review[]) => {
+      this.reviews = reviews;
+      const scores = [];
+
+      this.reviews.forEach(review => scores.push(review.score));
+      this.avarageScore =  this.calculateAverage(scores);
+    });
+  }
+
+  calculateAverage(scores) {
+    const average = arr => arr.reduce((p, c) => p + c, 0) / arr.length;
+
+    return average(scores)
   }
 
   ngOnInit() {
