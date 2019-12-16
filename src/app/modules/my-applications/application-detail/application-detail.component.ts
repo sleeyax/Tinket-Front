332081603b55e3af277073c0b1f4bf771f36c2c6
Lenny Kraaijenhof <hi@lenny.kr>
2019-12-16 01:53:40 +0100

    Application detail finished

--- src/app/modules/my-applications/application-detail/application-detail.component.ts
+++ src/app/modules/my-applications/application-detail/application-detail.component.ts
@@ -26,6 +26,22 @@ export class ApplicationDetailComponent implements OnInit {
       .subscribe((application) => this.application = application);
   }
 
+  get statusDescription() : String {
+    if(!this.application) return "";
+    if(this.application.contacted) return "Gefeliciteerd met je nieuwe opdracht! 🥳";
+    if(this.application.assignment.archivedAt) return "Opdracht afgerond.";
+    if(this.application.assignment.open) return "● Open voor inschrijvingen.";
+    return "● Gesloten voor inschrijvingen.";
+  }
+
+  get fullStatusDescription() : String {
+    if(!this.application) return "";
+    if(this.application.contacted) return "Laat je ons na afloop nog even weten wat je van de samenwerking vond?";
+    if(this.application.assignment.archivedAt) return "Het is niet meer mogelijk om je in te schrijven. Je werd niet gekozen voor deze opdracht.";
+    if(this.application.assignment.open) return "Nieuwe kandidaten kunnen zich nog steeds inschrijven voor deze opdracht.";
+    return "Het bedrijf heeft aangegeven dat deze opdracht niet langer nieuwe inschrijvingen ontvangt. Ontvangen inschrijvingen worden nog geëvalueerd.";
+  }
+
   ngOnInit() {
   }
 
