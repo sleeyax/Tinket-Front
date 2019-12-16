3bfa07f839cb9b9afdac857027b997942336f5ef
wesleyjanse <wesleyjanse@gmail.com>
2019-12-16 06:31:32 +0100

    Review bedrijf -> user

--- src/app/modules/my-assignments/assignment-applicant-detail/assignment-applicant-detail.component.ts
+++ src/app/modules/my-assignments/assignment-applicant-detail/assignment-applicant-detail.component.ts
@@ -6,6 +6,8 @@ import { AssignmentService } from '@app/core/services/assignment.service';
 import { ToastService } from '@app/core/services/toast.service';
 import * as moment from 'moment';
 import 'moment/locale/nl';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ReviewService } from '@app/core/services/review.service';
 
 @Component({
   selector: 'app-assignment-applicant-detail',
@@ -19,12 +21,17 @@ export class AssignmentApplicantDetailComponent implements OnInit {
   age = "";
   location = "";
   currentAssignmentId;
-
+  review = false;
+  submitted = false;
+  loading = false;
+  error = ""
   constructor(private router: Router,
     private route: ActivatedRoute,
     private assignmentService: AssignmentService,
+    private reviewService: ReviewService,
     private toastService: ToastService,
-    private userService: UserService) {
+    private userService: UserService,
+    private formBuilder: FormBuilder) {
 
     let userId = this.route.snapshot.paramMap.get("userId")
     this.currentAssignmentId = this.route.snapshot.paramMap.get("id")
@@ -47,7 +54,44 @@ export class AssignmentApplicantDetailComponent implements OnInit {
     return String(Math.abs(ageDate.getUTCFullYear() - 1970) + " jaar");
   }
 
+  reviewF() {
+    this.review = true;
+  }
+
   ngOnInit() {
+    this.inputForm = this.formBuilder.group({
+      message: ['', [Validators.required]],
+      anonymous: [false, [Validators.required]]
+    });
+  }
+
+  inputForm: FormGroup;
+  score = 0;
+  onSubmit() {
+    this.submitted = true;
+    this.loading = true
+    if (this.inputForm.valid) {
+      const review = {
+        anonymous: this.f.anonymous.value,
+        score: this.score,
+        reviewed: this.selectedUser._id,
+        description: this.f.message.value,
+      }
+      this.reviewService.postReview(review).subscribe(() => {
+        this.submitted = false;
+        this.loading = false;
+        this.toastService.toast("Bedankt voor je feedback!")
+      },
+        error => this.error = error)
+    } else {
+      this.submitted = false;
+      this.loading = false;
+    }
+  }
+  get f() { return this.inputForm.controls; }
+
+  vote(score) {
+    this.score = score.rating;
   }
 
 }
